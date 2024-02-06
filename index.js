71f096f1d0e7408ade82b7bdc33f33e1bed4c49d
Amisha Mishra <amishamishra12886@gmail.com>
2024-02-06 16:12:30 +0530

    middlewares-and-global-catches

--- index.js
+++ index.js
@@ -35,51 +35,85 @@ const food_items = [
       }
 ]
 
-app.use(express.json());
+app.use(express.json());    //middleware
+
+let numberOfRequests = 0;
+//middlewares
+function calculateRequests(req, res, next) {
+    numberOfRequests++;
+    console.log("Total requests till now: ",numberOfRequests);
+    next();
+}
 
 let food = []
-app.get("/", function(req, res) {
+app.get("/", calculateRequests, function(req, res) {
     food = []
+    let totalFoodItems = 0
     for(let i = 0; i < food_items.length; i++) {
         food.push(food_items[i].name);
+        totalFoodItems = i+1;
     }
 
     res.json({
-        food
+        food, totalFoodItems
     })
 })
 
-app.post("/", function(req, res) {
+app.post("/", calculateRequests, function(req, res) {
     const NAME = req.body.NAME;
     const NUTRIENTS = req.body.NUTRIENTS;
     const HEALTHY = req.body.HEALTHY;
-    food_items.push({
-        name: NAME,
-        nutrients: NUTRIENTS,
-        healthy: HEALTHY
-    })
-
-    res.json({
-        msg: "New food item added!"
-        
-    })
-})
-
-app.put("/", function(req, res) {
-    let { NAME, NUTRIENTS, HEALTHY } = req.body;
-    for(let i = 0; i < food_items.length; i++) {
-        if (NAME == food_items[i].name) {
-            food_items[i].name = NAME;
-            food_items[i].nutrients = NUTRIENTS;
-            food_items[i].healthy = HEALTHY;
-        }
+    
+    if(!NAME && !NUTRIENTS && !HEALTHY) {
+        res.json({
+            msg: "Give some input"
+        })
     }
-    res.json({msgg: "updated the required field"});
-    console.log(food_items);
+    else {
+        food_items.push({
+            name: NAME,
+            nutrients: NUTRIENTS,
+            healthy: HEALTHY
+        })
+    
+        res.json({
+            msg: "New food item added!"
+            
+        })
+    }
+    
 })
 
-app.delete("/", function(req, res) {
+app.put("/", calculateRequests, function(req, res) {
+    let { NAME, NUTRIENTS, HEALTHY } = req.body;
+
+    if(!NAME && !NUTRIENTS && !HEALTHY) {
+        res.json({
+            msg: "give input for update"
+        })
+    }
+    else {
+        for(let i = 0; i < food_items.length; i++) {
+            if (NAME == food_items[i].name) {
+                food_items[i].name = NAME;
+                food_items[i].nutrients = NUTRIENTS;
+                food_items[i].healthy = HEALTHY;
+            }
+        }
+        res.json({msgg: "updated the required field"});
+        console.log(food_items);
+    }
+})
+
+app.delete("/", calculateRequests, function(req, res) {
     let NAME = req.body.NAME;
+    if (!NAME) {
+        res.json({
+            msg: "enter item to be deleted"
+        })
+    }
+    let itemFound = false;
+
     for(let i = 0; i < food_items.length; i++) {
         if (NAME == food_items[i].name) {
             const index = food_items.findIndex(item => item.name === NAME);
@@ -88,13 +122,27 @@ app.delete("/", function(req, res) {
                 res.json({
                     msg: `Food item '${NAME}' deleted`
                   });
+                  itemFound = true;
             } 
-        } else {
-            res.status(404).json({
-                error: `Food item '${NAME}' not found`
-            });
         }
     }
+
+    if (!itemFound) {
+        res.status(404).json({
+            error: `Food item '${NAME}' not found`
+        });
+    }
+})
+
+let errcount = 0;
+//global catches
+//error handling middleware
+app.use(function(err, req, res, next) {
+    errcount++;
+    console.log("error count till now: ",errcount);
+    res.json({
+        msg: "Something's wrong"
+    })
 })
 
 app.listen(3000);
