83ecaf3b3d7509f4af8711ef8e964c7919481bc8
Amisha Mishra <amishamishra12886@gmail.com>
2024-02-09 13:16:36 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+//data
+const food_items = [
+    {
+        "name": "Apple",
+        "nutrients": ["vitamin_c", "fiber", "calories"],
+        "healthy": "Healthy"
+      },
+      {
+        "name": "Banana",
+        "nutrients": ["potassium", "vitamin_b6", "calories"],
+        "healthy": "Moderately Healthy"
+      },
+      {
+        "name": "Spinach",
+        "nutrients": ["iron", "vitamin_k", "fiber", "calories"],
+        "healthy": "Healthy"
+      },
+      {
+        "name": "Cold Drink",
+        "nutrients": ["sugar", "calories"],
+        "healthy": "Unhealthy"
+      },
+      {
+        "name": "Brown Rice",
+        "nutrients": ["fiber", "magnesium", "calories"],
+        "healthy": "Healthy"
+      },
+      {
+        "name": "Salmon",
+        "nutrients": ["omega_3", "protein", "vitamin_d", "calories"],
+        "healthy": "Moderately Healthy"
+      }
+]
+
+module.exports = {
+    food_items,
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const zod = require("zod");
 const { userExists, generateToken,authenticateUser} = require("./authorization");
+const {food_items} = require("./data");
 
 //express app initialize
 const app = express();
@@ -9,41 +10,6 @@ const app = express();
 //zod schema defined
 const schema_name_healthy = zod.string();
 const schema_nutrients = zod.array(zod.string())
-
-
-//data
-const food_items = [
-    {
-        "name": "Apple",
-        "nutrients": ["vitamin_c", "fiber", "calories"],
-        "healthy": "Healthy"
-      },
-      {
-        "name": "Banana",
-        "nutrients": ["potassium", "vitamin_b6", "calories"],
-        "healthy": "Moderately Healthy"
-      },
-      {
-        "name": "Spinach",
-        "nutrients": ["iron", "vitamin_k", "fiber", "calories"],
-        "healthy": "Healthy"
-      },
-      {
-        "name": "Cold Drink",
-        "nutrients": ["sugar", "calories"],
-        "healthy": "Unhealthy"
-      },
-      {
-        "name": "Brown Rice",
-        "nutrients": ["fiber", "magnesium", "calories"],
-        "healthy": "Healthy"
-      },
-      {
-        "name": "Salmon",
-        "nutrients": ["omega_3", "protein", "vitamin_d", "calories"],
-        "healthy": "Moderately Healthy"
-      }
-]
 
 //middleware (for getting json input from body)
 app.use(express.json());    
